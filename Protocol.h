fce6fe7a9513b0153ba555de260603edfc512e62
Sohal Patel <sohal.patel@franka.de>
2018-04-24 12:19:33 +0200

    Removed unused Gpio.c file and untabify Protocol.c, Protocol.h

--- Protocol.h
+++ Protocol.h
@@ -5,8 +5,8 @@
 * @brief Header file for state machine for bootloader protocol
 *
 *******************************************************************************/
-#ifndef PROTO_SM
-#define PROTO_SM
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
 /* ***************** Header / include files ( #include ) **********************/
 #include <stdint.h>
 #include <stddef.h>
@@ -20,31 +20,31 @@
 /* *************** Constant / macro definitions ( #define ) *******************/
 /* ********************* Type definitions ( typedef ) *************************/
 typedef union myCmd{
-	uint8_t 				bufferCMD[2];
-	eRESPONSE_ID 		returnValue;
-	eCOMMAND_ID 		receivedvalue;
+    uint8_t         bufferCMD[2];
+    eRESPONSE_ID    returnValue;
+    eCOMMAND_ID     receivedvalue;
 }tCmdUnion;
 
 typedef union myPayload{
-	tDATA_PACKET 		packet;
-	uint8_t 				bufferPLD[68];
+    tDATA_PACKET    packet;
+    uint8_t         bufferPLD[68];
 }tPldUnion;
 
 typedef union myAppData{
-	tFIRMWARE_PARAM Firmware;
-	uint8_t 				bufferData[4];
+    tFIRMWARE_PARAM Firmware;
+    uint8_t         bufferData[4];
 }tAppDataUnion;
 
 typedef enum {
-	eDefaultState = 0,
-	eFlashEraseCMD,
-	eWriteMemory,
-	ePayloadReceive,
-	ePayloadCheck,
-	eWriteAppCRC,
-	eFinishUpdate,
-	eFlashVerifyApplication,
-	eStartAppCMD
+    eDefaultState = 0,
+    eFlashEraseCMD,
+    eWriteMemory,
+    ePayloadReceive,
+    ePayloadCheck,
+    eWriteAppCRC,
+    eFinishUpdate,
+    eFlashVerifyApplication,
+    eStartAppCMD
 }tProtoState;
 /* ***************** Global data declarations ( extern ) **********************/
 /* ***** External parameter / constant declarations ( extern const ) **********/
